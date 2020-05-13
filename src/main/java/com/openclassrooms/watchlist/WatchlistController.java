bc17477f18588cd525ed6c4b903fccfeb9d2670c
kiran.karki <kiran.karki@student.fh-kiel.de>
2020-05-13 13:52:43 +0200

    form that submit add new movie to watchlist items

--- src/main/java/com/openclassrooms/watchlist/WatchlistController.java
+++ src/main/java/com/openclassrooms/watchlist/WatchlistController.java
@@ -9,33 +9,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 public class WatchlistController {
 
     List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
 
-    @GetMapping("/watchlist")
+    @RequestMapping(value="/watchlist", method=RequestMethod.GET)
     public ModelAndView getList() {
 
         String viewName = "watchlist";
         Map<String, Object> model = new HashMap<String, Object>();
-
+/*
         watchlistItems.clear();
         watchlistItems.add(new WatchlistItem(1, "Lion King","8.5","high","Hakuna matata!"));
         watchlistItems.add(new WatchlistItem(2, "Frozen","7.5","medium","Let it go!"));
         watchlistItems.add(new WatchlistItem(3, "Cars","7.1","low","Go go go!"));
-        watchlistItems.add(new WatchlistItem(4, "Wall-E","8.4","high","You are crying!"));
+        watchlistItems.add(new WatchlistItem(4, "Wall-E","8.4","high","You are crying!"));*/
 
         model.put("watchlistItems", watchlistItems);
         model.put("numberOfMovies", watchlistItems.size());
 
         return new ModelAndView(viewName , model);
     }
-    @GetMapping("/watchlistItemForm")
+    @RequestMapping(value="/watchlistItemForm", method=RequestMethod.GET)
     public ModelAndView showWatchlistItemForm() {
 
         String viewName = "watchlistItemForm";
@@ -47,10 +48,10 @@ public class WatchlistController {
         return new ModelAndView(viewName,model);
     }
 
-    @PostMapping("/watchlistItemForm")
-    public ModelAndView submitWatchlistItemForm(WatchlistItem watchlistItem) {
-        watchlistItem.setId(watchlistItem.index++);
-        watchlistItems.add(watchlistItem);
+    @RequestMapping(value="/watchlistItemForm", method=RequestMethod.POST)
+    public ModelAndView submitWatchlistItemForm(WatchlistItem watchlist) {
+       watchlist.setId(watchlist.index++);
+        watchlistItems.add(watchlist);
 
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl("/watchlist");
